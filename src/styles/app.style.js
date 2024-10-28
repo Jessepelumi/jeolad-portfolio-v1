affb3c95aaf30dc9c4af6d0ae7abfc20e4568b9a
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-10-28 11:25:50 +0100

    changed bg color

--- src/styles/app.style.js
+++ src/styles/app.style.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
 export const StyledApp = styled.div`
-  background-color: #000000;
+  background-color: var(--background);
   -webkit-tap-highlight-color: transparent;
 `;
