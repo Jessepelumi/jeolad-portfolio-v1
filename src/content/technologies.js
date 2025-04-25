d5c25521a11940183b68b661700204d59efbafb3
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-25 16:25:32 +0100

    updated the technologies I've worked with

---
+++ src/content/technologies.js
@@ -0,0 +1,10 @@
+export const technologies = [
+  { id: 1, name: "JavaScript & TypeScript" },
+  { id: 2, name: "React & Next.js" },
+  { id: 3, name: "Node.js & Express" },
+  { id: 4, name: "React Native" },
+  { id: 5, name: "Flutter" },
+  { id: 6, name: "Python" },
+  { id: 7, name: "Git & GitHub" },
+  { id: 8, name: "PostgreSQL, MySQL, & MongoDB" },
+];
