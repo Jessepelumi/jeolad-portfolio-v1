9a520d9378013781c9733d37bbc76e6263075eba
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-25 19:50:45 +0100

    added start and end date attributes. Modified job title and description

---
+++ src/content/companies.js
@@ -0,0 +1,16 @@
+export const companies = [
+  {
+    id: 1,
+    companyName: "Hire NG",
+    link: "https://www.linkedin.com/company/hire-ng/",
+    title: "Mobile Engineer",
+    startDate: "May 2024",
+    endDate: "Feb 2025",
+    details: [
+      "Contributed to a mobile application that connects over 50k monthly active users (MAUs), enabling seamless interaction between clients and artisans using Dart & Flutter.",
+      "Optimized app performance by 20% through the implementation of Riverpod for efficient state management.",
+      "Developed scalable, reusable UI components that enhanced usability and maintained design consistency.",
+      "Built core in-app mobile wallet features including transaction history, funding, and withdrawal functionalities.",
+    ],
+  },
+];
