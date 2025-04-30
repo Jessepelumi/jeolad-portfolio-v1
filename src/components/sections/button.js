8ba7a10dd07dffa6076a85d0928d60ff5cd44f64
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2025-04-30 14:56:05 +0100

    added react link for internal routing

--- src/components/sections/button.js
+++ src/components/sections/button.js
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom";
 import { StyledButton } from "../../styles/button.style";
 
-const Button = ({ text, link }) => {
+const Button = ({ variant = "external", text, link }) => {
+  if (variant === "internal") {
+    return (
+      <Link to={link}>
+        <StyledButton>{text}</StyledButton>
+      </Link>
+    );
+  }
+
   return (
     <a href={process.env.PUBLIC_URL + link} target="_blank" rel="noreferrer">
       <StyledButton>{text}</StyledButton>
